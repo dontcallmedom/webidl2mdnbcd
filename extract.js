a3f8d0c42a37d7973d5a64d6cab7dc77de89ae0f
Dominique Hazael-Massieux <dom@w3.org>
2018-03-16 11:49:00 +0100

    Sort BCD file with properties first, and then alphabetically by name

--- extract.js
+++ extract.js
@@ -2,10 +2,6 @@ const {extract} = require("reffy/extract-webidl");
 const {parse} = require("reffy/parse-webidl");
 const fs = require("fs");
 
-const skeleton = {
-  api: {}
-};
-
 const base_mdn_url = "https://developer.mozilla.org/docs/Web/API/";
 
 const bcd_skeleton = {
@@ -55,21 +51,26 @@ const bcd_skeleton = {
     }
 };
 
+const propertiesFirst = (a,b) => a.type === b.type ? a.name.localeCompare(b.name) :  (a.type === "attribute" ? -1 : 1);
+
 extract(process.argv[2])
   .then(parse)
   .then(({idlNames}) => {
     Object.keys(idlNames).filter(n => idlNames[n].type === "interface")
       .forEach(interface => {
-        const bcd = {...skeleton};
+        const bcd = {api:{}};
         bcd.api[interface] = {};
         bcd.api[interface].__compat = {...bcd_skeleton};
         bcd.api[interface].__compat.mdn_url = base_mdn_url + interface;
         idlNames[interface].members.filter(m => m.name)
+          .sort(propertiesFirst)
           .forEach(m => {
             bcd.api[interface][m.name] = {};
             bcd.api[interface][m.name].__compat = {...bcd_skeleton};
             bcd.api[interface][m.name].__compat.mdn_url = base_mdn_url + interface + "/" + m.name;
           });
+        // TODO:
+        // check if file already exists
         fs.writeFileSync(interface + ".json", JSON.stringify(bcd, null, 2));
       });
 
